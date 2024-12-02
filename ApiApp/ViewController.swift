e11b6f0da8357ff9552070775c698e8f8e54bf8c
sumire.kuroda <sumire.a.kuroda@fujifilm.com>
2024-12-02 12:23:06 +0900

    Initial Commit

---
+++ ApiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiApp
+//
+//  Created by UI3 on 2024/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
